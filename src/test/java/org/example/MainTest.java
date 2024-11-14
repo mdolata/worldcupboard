17d4c180a7b5ad94bd254fdcf97fdf2eb40c7111
mateusz_d <mateusz.dolata2@gmail.com>
2024-11-14 22:05:51 +0100

    added gradle

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void shouldPass() {
+        assertTrue(true);
+    }
+
+    @Test
+    void shouldFail() {
+        assertFalse(true);
+    }
+}
